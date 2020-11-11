4dcfdf9a6da13d8afd75572c87926adc216c62d0
adriannborella@hotmail.com <adriannborella@hotmail.com>
2020-11-10 22:37:08 -0300

    Init

---
+++ __manifest__.py
@@ -0,0 +1,42 @@
+{
+    'name': 'appointments',
+    'summary': 'appointments Module Project',
+    'version': '1.0',
+
+    'description': """
+appointments Module Project.
+==============================================
+
+
+    """,
+
+    'author': 'Adrian Borella',
+    'website': 'https://github.com/adriannborella',
+    'license': 'AGPL-3',
+    
+    'category': 'Uncategorized',
+
+    'depends': [
+        'base',
+    ],
+    'external_dependencies': {
+        'python': [
+        ],
+    },
+    'data': [
+    ],
+    'demo': [
+    ],
+    'js': [
+    ],
+    'css': [
+    ],
+    'qweb': [
+    ],
+    'images': [
+    ],
+    'test': [
+    ],
+
+    'installable': True
+}
