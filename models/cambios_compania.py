4695ef815a83901c997e039fb3b1cfbfa7cb8b8f
German Chiocchia <germanchiocchia@gmail.com>
2020-11-22 17:19:56 -0300

    company

--- models/cambios_compania.py
+++ models/cambios_compania.py
@@ -7,16 +7,25 @@ class asw_cambios_compania(models.Model):
 
     appointment_qty = fields.Integer(
         string=u'Cantidad de turnos',
+        #compute='_compute_appointment_qty',
     )
 
     appointment_duration = fields.Integer(
         string=u'Duración del turno',
+        compute='_compute_appointment_duration',
     )
 
     start_time = fields.Float(
         string=u'Hora de inicio',
+        required='True',            #depende de que campos son calculados!
     )
 
     end_time = fields.Float(
         string=u'Hora de fin',
     )
+
+    @api.depends('start_time','end_time')
+    def _compute_appointment_duration(self):
+        for record in self:
+            duration = record.end_time - record.start_time
+            record.appointment_duration = duration
