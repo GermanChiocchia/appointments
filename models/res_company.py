07d24f7c46a04e85508ffe12b6e431e007f18ab7
adriannborella@hotmail.com <adriannborella@hotmail.com>
2020-11-26 10:31:42 -0300

    [FIX] Correcciones generales

--- models/res_company.py
+++ models/res_company.py
@@ -11,21 +11,16 @@ from odoo.exceptions import UserError, ValidationError
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    timeframes_ids = fields.One2many(string=u'Horarios',comodel_name='appointments.appointment',inverse_name='company_id')
-
-class AppointmentsAppointment(models.Model):
-    _name = 'appointments.appointment'
-    _description = 'Horarios'
-
-    company_id = fields.Many2one(string=u'Compania',comodel_name='res.company',ondelete='set null',default='self.env.user.company_id')
-    day = fields.Char(string=u'Dia')
-    enabled = fields.Boolean(string=u'Habilitado')
-
-class AppointmentsCambioCompania(models.Model):
-    _name = 'appointments.cambio.compania'
-    _description = 'Cambios compania'
-
+    timeframes_ids = fields.One2many(string=u'Horarios',comodel_name='appointments.timeframe',inverse_name='company_id')
     appointment_qty = fields.Integer(string=u'Cantidad de turnos')
     appointment_duration = fields.Integer(string=u'Duración del turno')
     start_time = fields.Float(string=u'Hora de inicio')
     end_time = fields.Float(string=u'Hora de fin')
+
+class AppointmentsTimeFrames(models.Model):
+    _name = 'appointments.timeframe'
+    _description = 'Horarios'
+
+    company_id = fields.Many2one(string=u'Compania',comodel_name='res.company',ondelete='set null')
+    day = fields.Char(string=u'Dia')
+    enabled = fields.Boolean(string=u'Habilitado')
