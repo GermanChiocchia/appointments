3f277e561962169dba6be3af5eee01dcc3bdf30e
German Chiocchia <germanchiocchia@gmail.com>
2020-11-26 12:40:57 -0300

    a punto para trabajar

--- models/res_company.py
+++ models/res_company.py
@@ -11,8 +11,6 @@ from odoo.exceptions import UserError, ValidationError
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    timeframes_ids = fields.One2many(string=u'Horarios',comodel_name='appointments.appointment',inverse_name='company_id')
-    cambio_compania_ids = fields.One2many(string=u'Cambios compania',comodel_name='appointments.cambio.compania',inverse_name='company_id')
     timeframes_ids = fields.One2many(string=u'Horarios',comodel_name='appointments.timeframe',inverse_name='company_id')
     appointment_qty = fields.Integer(string=u'Cantidad de turnos Simultaneos')
     appointment_duration = fields.Integer(string=u'Duración del turno')
@@ -25,15 +23,4 @@ class AppointmentsTimeFrames(models.Model):
 
     company_id = fields.Many2one(string=u'Compania',comodel_name='res.company',ondelete='set null')
     day = fields.Char(string=u'Dia')
-    enabled = fields.Boolean(string=u'Habilitado')
-
-class AppointmentsCambioCompania(models.Model):
-    _name = 'appointments.cambio.compania'
-    _description = 'Cambios compania'
-
-    company_id = fields.Many2one(string=u'Compania',comodel_name='res.company',ondelete='set null',default='self.env.user.company_id')
-    appointment_qty = fields.Integer(string=u'Cantidad de turnos')
-    appointment_duration = fields.Integer(string=u'Duración del turno')
-    start_time = fields.Float(string=u'Hora de inicio')
-    end_time = fields.Float(string=u'Hora de fin')
     enabled = fields.Boolean(string=u'Habilitado')
